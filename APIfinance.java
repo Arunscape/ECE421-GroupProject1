f6940ff9460b9523ee851b466336313c786c12c9
arostron <alex.rostron24@gmail.com>
2020-01-06 14:24:26 -0700

    Add api key to source java file

---
+++ APIfinance.java
@@ -0,0 +1,20 @@
+private static final String BASE_URL = "https://www.alphavantage.co/query?";
+private final static String apiKey = "X5FCG839K0RIP3DQ";
+
+public static BigDecimal getPrice(final String symbol) { 
+	BigDecimal price = new BigDecimal(0);
+	try {
+			URL url = new URL(BASE_URL + "function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=" + apiKey);
+			URLConnection connection = url.openConnection();
+			InputStreamReader inputStream = new InputStreamReader(connection.getInputStream(), "UTF-8");
+			BufferedReader bufferedReader = new BufferedReader(inputStream);
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+					if (line.contains("price")) {
+							price = new BigDecimal(line.split("\"")[3].trim());
+					} }
+			bufferedReader.close(); } catch (IOException e) {
+					System.out.println("failure sending request");
+			}
+	return price; 
+}
