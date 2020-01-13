ddc872edce9da87f98a5d79cc63b0e4a5b786b6e
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-01-13 15:49:47 -0700

    filter under 500

--- src/PickShareFunctional.java
+++ src/PickShareFunctional.java
@@ -1,11 +1,17 @@
+import java.util.stream.Stream;
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
 public class PickShareFunctional {
   
-  public static String findHighPriced(Stream prices){
+  public static String findHighPriced(Stream<String> prices){
     return prices
-            .map( (symbol) -> Pair<String, BigInteger>(symbol, APIFinance.getPrice(symbol)))
-            .max((a, b) -> Math.max(a.getValue(), b.getValue())
-            .getKey();
+            .map( (symbol) -> new ShareInfo(symbol, APIFinance.getPrice(symbol)))
+            .filter( (shareinfo) -> shareinfo.price.compareTo(new BigDecimal(500)) < 0)
+            .max((a, b) -> a.price.compareTo(b.price))
+            .orElseThrow(NoSuchElementException::new)
+            .symbol;
       
     }
-  }
 }
