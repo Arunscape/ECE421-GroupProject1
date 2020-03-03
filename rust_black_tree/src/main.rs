316bba25d585d193db55b799227513a9857b31c2
Jacob Reckhard <jacobreckhard@gmail.com>
2020-03-03 09:58:00 -0700

    added to report

--- rust_black_tree/src/main.rs
+++ rust_black_tree/src/main.rs
@@ -211,7 +211,7 @@ fn eval(
             *bs = BSTree::new();
         }
         Cmd::NumberError => {
-            println!("Only signed word sized numbers are supported in the demo. Other datatypes can be purchased for $5.99.");
+            println!("Only unsigned word sized numbers are supported in the demo. Other datatypes can be purchased for $5.99.");
         }
     }
 }
