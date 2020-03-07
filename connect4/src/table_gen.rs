3f4093a75ca14cf6252e8b28c528212999ddb444
Jacob Reckhard <jacobreckhard@gmail.com>
2020-03-07 13:08:58 -0700

    minmax

--- connect4/src/table_gen.rs
+++ connect4/src/table_gen.rs
@@ -8,20 +8,39 @@ use connect_game::game::*;
 use connect_game::bitboard::*;
 
 fn main() {
-    gen_table(4, 4, connect4::checker)
+    gen_table(7, 6, connect4::checker)
 }
 
 fn gen_table(width: usize, height: usize, checker: fn(&Game) -> BoardState) {
-    let board = connect_game::game::Board::new(4, 4);
-    let mut game = connect_game::game::Game::new(board, Connect4);
-    let mut data = HashMap::new();
+    let board = connect_game::game::Board::new(width, height);
+    let game = connect_game::game::Game::new(board, Connect4);
 
     let mut bb = BitBoard64::from_game(&game);
-    evaluate_board(&mut bb, &mut data);
-    println!("{} items in table", data.len())
+    let s = (bb.size() / 2) as isize;
+    let res = evaluate_board(&mut bb, -s, s, 19);
+    unsafe {
+        println!(
+            "res is {}. searched {} states",
+            res,
+            STATES,
+        )
+    }
 }
 
-fn calc_evaluate_board(game: &mut BitBoard64, data: &mut HashMap<u64, i8>) -> isize {
+static mut STATES: usize = 0;
+fn evaluate_board(
+    game: &mut BitBoard64,
+    alpha: isize,
+    beta: isize,
+    depth: usize,
+) -> isize {
+    unsafe {
+        STATES += 1;
+    }
+    if depth == 0 {
+        return 0;
+    }
+
     if game.get_turns() == game.size() {
         return 0; // Draw game, 0 score
     }
@@ -33,34 +52,31 @@ fn calc_evaluate_board(game: &mut BitBoard64, data: &mut HashMap<u64, i8>) -> is
         }
     }
 
-    let mut score = std::isize::MIN;
+    let mut beta = beta;
+    let mut alpha = alpha;
+    let top = ((game.size() - 1 - game.get_turns()) / 2) as isize;
+    if beta > top {
+        beta = top;
+        if alpha >= beta {
+            return beta;
+        }
+    }
+
     for x in 0..game.width {
         if game.can_play(x) {
             let (p, m) = game.get_pos_mask();
             game.play(x);
-            score = std::cmp::max(-evaluate_board(game, data), score);
+            let score = -evaluate_board(game, -beta, -alpha, depth - 1);
             game.undo_to(p, m);
+
+            if score >= beta {
+                return score;
+            }
+            if score > alpha {
+                alpha = score;
+            }
         }
     }
-    score
+    alpha
 }
 
-fn evaluate_board(game: &mut BitBoard64, data: &mut HashMap<u64, i8>) -> isize {
-    let k1 = game.flip_color().key();
-    let k2 = game.flip_x().key();
-    let k3 = game.flip_color().key();
-    let k4 = game.flip_x().key();
-    if let Some(score) = data.get(&k1) {
-        -(*score) as isize
-    } else if let Some(score) = data.get(&k2) {
-        -(*score) as isize
-    } else if let Some(score) = data.get(&k3) {
-        (*score) as isize
-    } else if let Some(score) = data.get(&k4) {
-        (*score) as isize
-    } else {
-        let score = calc_evaluate_board(game, data);
-        data.insert(game.key(), score as i8);
-        score
-    }
-}
