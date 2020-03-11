9849427a627d9bd43fc332e454f696857639f7b2
Jacob Reckhard <jacobreckhard@gmail.com>
2020-03-11 12:35:15 -0600

    made it play toot and otto again

--- connect4/src/main.rs
+++ connect4/src/main.rs
@@ -1,69 +1,86 @@
-use connect_game::game::{Checker, ChipDescrip, Player, Game, check_pattern};
+use connect_game::game::{check_pattern, Checker, ChipDescrip, Game, Player};
 
+use connect_game::io::{BLK, BRIGHTEN, FILLED, RED, YEL};
 
-use connect_game::io::{BLK, BRIGHTEN, YEL, RED, FILLED};
-
-pub fn connect4_yellow() -> ChipDescrip {
-    ChipDescrip {
-        bg_color: BLK + BRIGHTEN,
-        fg_color: YEL,
-        graphic: FILLED,
-    }
+pub fn four_in_a_row(chip: ChipDescrip) -> Checker {
+    let check = move |game: &Game| -> bool { check_pattern(&vec![chip; 4], game) };
+    Box::from(check)
 }
 
-pub fn connect4_red() -> ChipDescrip {
-    ChipDescrip {
+pub fn wrap_4_check(chip: ChipDescrip, chip_inner: ChipDescrip) -> Checker {
+    let check = move |game: &Game| -> bool {
+        check_pattern(&vec![chip, chip_inner, chip_inner, chip], game)
+    };
+    Box::from(check)
+}
+
+fn connect4() -> Game {
+    let board = connect_game::game::Board::new(7, 6);
+
+    // setup chip types
+    let red = ChipDescrip {
         bg_color: BLK + BRIGHTEN,
         fg_color: RED,
         graphic: FILLED,
-    }
+    };
+    let yellow = ChipDescrip {
+        bg_color: BLK + BRIGHTEN,
+        fg_color: YEL,
+        graphic: FILLED,
+    };
+
+    let players = vec![
+        Player {
+            chip_options: vec![red],
+            win_conditions: vec![four_in_a_row(red)],
+        },
+        Player {
+            chip_options: vec![yellow],
+            win_conditions: vec![four_in_a_row(yellow)],
+        },
+    ];
+
+    connect_game::game::Game::new(board, players)
 }
 
-pub fn four_in_a_row_red() -> Checker {
-    fn check(game: &Game) -> bool {
-        check_pattern(&vec![connect4_red(); 4], game)
-    }
-    Box::from(check)
-}
-pub fn four_in_a_row_yellow() -> Checker {
-    fn check(game: &Game) -> bool {
-        check_pattern(&vec![connect4_yellow(); 4], game)
-    }
-    Box::from(check)
-}
-
-fn connect4() {
-    let board = connect_game::game::Board::new(7, 6);
-
-    let red = connect4_red();
-    let yellow = connect4_yellow();
-
-    let mut game = connect_game::game::Game::new(
-        board,
-        vec![
-            Player {
-                chip_options: vec![red],
-                win_conditions: vec![four_in_a_row_red()],
-            },
-            Player {
-                chip_options: vec![yellow],
-                win_conditions: vec![four_in_a_row_yellow()],
-            },
-        ],
-    );
-    connect_game::play(&mut game)
-}
-
-fn toto() {
+fn toto() -> Game {
     let board = connect_game::game::Board::new(6, 4);
-    // let mut game = connect_game::game::Game::new(board, Toto);
-    // connect_game::play(&mut game)
+
+    // setup chip types
+    let chip_t = ChipDescrip {
+        bg_color: BLK + BRIGHTEN,
+        fg_color: RED,
+        graphic: 't',
+    };
+    let chip_o = ChipDescrip {
+        bg_color: BLK + BRIGHTEN,
+        fg_color: YEL,
+        graphic: 'o',
+    };
+
+    let players = vec![
+        Player {
+            chip_options: vec![chip_t, chip_o],
+            win_conditions: vec![wrap_4_check(chip_t, chip_o)],
+        },
+        Player {
+            chip_options: vec![chip_t, chip_o],
+            win_conditions: vec![wrap_4_check(chip_o, chip_t)],
+        },
+    ];
+
+    connect_game::game::Game::new(board, players)
 }
 
 fn main() {
-    if std::env::args().len() > 1 {
-        toto();
+    let args: Vec<String> = std::env::args().collect();
+    let mut game = if args.len() > 1 {
+        match args[1].as_ref() {
+            "toto" => toto(),
+            _ => connect4(),
+        }
     } else {
-        connect4();
-    }
+        connect4()
+    };
+    connect_game::play(&mut game)
 }
