71b9837f3103c0399178d2802040fe38096dbe55
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-04-07 21:49:15 -0600

    server split

--- connect4/connect4-server/main.js
+++
@@ -1,6 +0,0 @@
-import init, { run_app } from './pkg/connect_game.js';
-async function main() {
-   await init('/pkg/connect_game_bg.wasm');
-   run_app();
-}
-main()
--- connect4/connect4-server/src/main.rs
+++ connect4/connect4-server/src/main.rs
@@ -1,19 +1,46 @@
-use connect_game::games::*;
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::NamedFile;
+use rocket_contrib::serve::StaticFiles;
+use std::io;
+
+/// /signin: takes username and password, returns JWT
+#[get("/signin")]
+fn signin() -> &'static str {
+    "Signin"
+}
+
+/// /playmove: takes in description of move, gameid, and JWT, returns new gamestate
+#[get("/playmove")]
+fn playmove() -> &'static str {
+    "playmove"
+}
+
+/// /refresh: takes in JWT returns new JWT
+#[get("/refresh")]
+fn refresh() -> &'static str {
+    "refresh"
+}
+
+/// /creategame: takes in description of game, and JWT, returns gameid
+#[get("/creategame")]
+fn creategame() -> &'static str {
+    "creategame"
+}
+
+/// /getgame: takes in gameid, JWT, and returns gamestate
+#[get("/getgame")]
+fn getgame() -> &'static str {
+    "getgame"
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![signin, playmove, refresh, creategame, getgame])
+}
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let mut game = if args.len() > 1 {
-        match args[1].as_ref() {
-            "toto" => toto(),
-            "toto_ai" => toto_ai(),
-            "3" => connect4_3player(),
-            "ai" => connect4_ai(),
-            "aibig" => connect4_large_ai(),
-            "ai2" => connect4_ai_p2(),
-            _ => connect4(),
-        }
-    } else {
-        connect4()
-    };
-    connect_game::play(&mut game, connect_game::io::TermIO::new())
+    rocket().launch();
 }
