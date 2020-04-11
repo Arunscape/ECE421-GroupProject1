6491ea8e902c1754d020ca9ffb74f955c4125b15
arostron <alex.rostron24@gmail.com>
2020-04-10 18:59:26 -0600

    Add lib objects

--- connect4/connect4-server/src/games.rs
+++ connect4/connect4-server/src/games.rs
@@ -3,10 +3,15 @@ use crate::dbhelper::*;
 use bson::doc;
 use serde::{Deserialize, Serialize};
 
+use connect4_lib::{
+    game, game::Board, game::BoardState, game::ChipDescrip, game::Game, games, io, GameIO,
+};
+
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Game {
-    data: String,
+struct GameData {
+    game: game::Game,
+    board_state: game::BoardState,
 }
 
 #[cfg(test)]
