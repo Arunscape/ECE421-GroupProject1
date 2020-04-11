1af1c0eda5df3964ddb9f131cb9f8b6ba7fbe377
arostron <alex.rostron24@gmail.com>
2020-04-11 17:10:29 -0600

    Add GameData to comms types

--- connect4/connect4-coms/src/lib.rs
+++ connect4/connect4-coms/src/lib.rs
@@ -1,9 +1,22 @@
 pub mod types {
     use serde::{Deserialize, Serialize};
+    use connect4_lib::game::{BoardState, Game};
 
     #[derive(Debug, Serialize, Deserialize)]
     pub struct Signin{
         pub status: String,
         pub tok: String,
     }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct GameData {
+        roomcode: String,
+        board_state: BoardState,
+        users: Vec<String>,
+
+        #[serde(flatten)]
+        game: Game,
+    }
+
 }
+
