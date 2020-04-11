1777625d4cdce6831142858110a8f9c9911aa218
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-04-11 17:32:05 -0600

    refactor coms

---
+++ connect4/connect4-coms/src/types.rs
@@ -0,0 +1,27 @@
+use connect4_lib::game::ChipDescrip;
+use connect4_lib::game::{BoardState, Game};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Signin {
+    pub status: String,
+    pub tok: String,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PlayMove {
+    pub status: String,
+    pub game_id: String,
+    pub column: isize,
+    pub chip_descrip: ChipDescrip,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GameData {
+    pub status: String,
+    roomcode: String,
+    board_state: BoardState,
+    users: Vec<String>,
+
+    #[serde(flatten)]
+    game: Game,
+}
