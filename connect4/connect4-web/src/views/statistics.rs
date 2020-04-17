3a60f231bb54bddbbfc3b6a1fe48395e346ff97e
Jacob Reckhard <jacobreckhard@gmail.com>
2020-04-17 15:53:32 -0600

    stats bug

--- connect4/connect4-web/src/views/statistics.rs
+++ connect4/connect4-web/src/views/statistics.rs
@@ -1,7 +1,7 @@
 use yew::prelude::*;
 use yew::virtual_dom::VNode;
 
-use crate::components::Menu;
+use crate::components::MenuButtonLight;
 use crate::coms;
 use connect4_coms::types::GameStats;
 use wasm_bindgen_futures::spawn_local;
@@ -51,15 +51,22 @@ impl Component for Statistics {
 
     fn view(&self) -> VNode {
         html! {
-            <Menu title=format!("Statistics") topbar="" show_settings=false show_stats=false>
-              <div>
-                <h2>{format!("Games won: {}", self.stats.games_won)}</h2>
-                <h2>{format!("Games lost: {}", self.stats.games_lost)}</h2>
-                <h2>{format!("Games drawed: {}", self.stats.games_drawed)}</h2>
-                <h2>{format!("Games ongoing: {}", self.stats.games_ongoing)}</h2>
-                <h2>{format!("Games completed: {}", self.stats.games_won + self.stats.games_lost + self.stats.games_drawed)}</h2>
-              </div>
-            </Menu>
+            <div class="h-full flex flex-col items-center justify-between">
+                <div class="w-full flex px-5 justify-between">
+                  <p></p>
+                  <MenuButtonLight text="home" dest="/"/>
+                </div>
+                <h1 class="font-comic text-6xl">{ "Statistics" }</h1>
+                <div>
+                  <h2>{format!("Games won: {}", self.stats.games_won)}</h2>
+                  <h2>{format!("Games lost: {}", self.stats.games_lost)}</h2>
+                  <h2>{format!("Games drawed: {}", self.stats.games_drawed)}</h2>
+                  <h2>{format!("Games ongoing: {}", self.stats.games_ongoing)}</h2>
+                  <h2>{format!("Games completed: {}", self.stats.games_won + self.stats.games_lost + self.stats.games_drawed)}</h2>
+                </div>
+                <div class="w-full flex md:justify-end">
+                </div>
+            </div>
         }
     }
 }
